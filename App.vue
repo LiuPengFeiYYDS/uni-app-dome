d90840f1ccedb7f2fb17674532d3a741717a67cb
lpf <2792717765@qq.com>
2022-08-15 19:12:13 +0800

    '引入全局的公共样式'

--- App.vue
+++ App.vue
@@ -13,5 +13,9 @@
 </script>
 
 <style>
-	/*每个页面公共css */
+	/*每个页面公共css 引入全局的样式*/
+	@import url(@/common/iconfont/iconfont.css);
+	@import url(@/common/styles/animate.min.css);
+	@import url(@/common/styles/common.css);
+	@import url(@/common/styles/free.css);
 </style>
