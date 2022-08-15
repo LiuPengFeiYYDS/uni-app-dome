d5bd621a8ac152c3b759003c81755807dd136c2e
lpf <2792717765@qq.com>
2022-08-15 19:58:01 +0800

    '写搜索框'

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
-	<view class="content">
-	
+	<view class="container">
+		<i-search-bar></i-search-bar>
 	</view>
 </template>
 
@@ -21,29 +21,5 @@
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+	
 </style>
